3522682907fcdef28ba53bb1b696eead7493ef76
EthanPerello <115740777+EthanPerello@users.noreply.github.com>
2025-04-06 20:19:36 -0400

    Add Dread Halls Unity project files

---
+++ Assets/DespawnOnHeight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <= -5) {
+            GameObject.Destroy(GameObject.Find("WhisperSource"));
+            LevelCounter.mazeNumber = 0;
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
---
+++ Assets/LevelCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCounter : MonoBehaviour
+{
+    public Text levelText;
+    public static int mazeNumber = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelText.text = "Maze " + mazeNumber.ToString();
+    }
+}
